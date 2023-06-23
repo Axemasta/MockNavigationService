f1aa8ad57a1bc4bb3c71a99abb516e4ec59614e2
Axemasta <33064621+Axemasta@users.noreply.github.com>
2023-06-23 16:40:07 +0100

    Implement Setup Tests
    Added test cases for setup, I dont think theres total coverage but its mostly there. I can come back later to test all branches since the code is a tad spaghet 😇

--- src/MockNavigationService/Extensions/SetupExtension.cs
+++ src/MockNavigationService/Extensions/SetupExtension.cs
@@ -5,36 +5,86 @@ namespace MockNavigationService.Extensions;
 
 public static class SetupExtension
 {
-    public static void SetupNavigationResult(this MockNavigationService mockNavigationService, string destination, bool success)
+    /// <summary>
+    /// Setup Navigation Result.
+    /// This is the simple setup and will only populate Success
+    /// </summary>
+    /// <param name="mockNavigationService">Instance of mock navigation service to setup</param>
+    /// <param name="destination">The navigation target</param>
+    /// <param name="success">Whether the navigation suceeds</param>
+    /// <param name="navigationParameters">The navigation parameters for navigation</param>
+    public static void SetupNavigationResult(this MockNavigationService mockNavigationService, string destination, bool success, INavigationParameters? navigationParameters = null)
     {
-        var mockResult = new Mock<INavigationResult>();
-
-        mockResult.SetupGet(m => m.Success)
-            .Returns(success);
-
-        mockNavigationService.Setup(
-            m => m.NavigateAsync(
-                It.Is<Uri>(u => u.Equals(destination)),
-                It.IsAny<INavigationParameters>()))
-            .ReturnsAsync(mockResult.Object);
+        SetupNavigationResultInternal(mockNavigationService, destination, success, navigationParameters);
     }
 
-    public static void SetupNavigationResult(this MockNavigationService mockNavigationService, string destination, bool success, INavigationParameters navigationParameters)
+    /// <summary>
+    /// Setup Navigation Result With Exception.
+    /// This will set Success to false and populate the exception property of the navigation result.
+    /// </summary>
+    /// <param name="mockNavigationService">Instance of mock navigation service to setup</param>
+    /// <param name="destination">The navigation target</param>
+    /// <param name="exception">The exception to throw during navigation</param>
+    /// <param name="navigationParameters">The navigation parameters for navigation</param>
+    public static void SetupNavigationResult(this MockNavigationService mockNavigationService, string destination, Exception exception, INavigationParameters? navigationParameters = null)
+    {
+        SetupNavigationResultInternal(mockNavigationService, destination, false, navigationParameters, exception);
+    }
+
+    /// <summary>
+    /// Setup Navigation Result With Exception.
+    /// This will set Success to false and populate the exception property of the navigation result.
+    /// Cancelled will be set according to the given parameter.
+    /// </summary>
+    /// <param name="mockNavigationService">Instance of mock navigation service to setup</param>
+    /// <param name="destination">The navigation target</param>
+    /// <param name="cancelled">Whether the navigation was cancelled</param>
+    /// <param name="exception">The exception to throw during navigation</param>
+    /// <param name="navigationParameters"><The navigation parameters for navigation/param>
+    public static void SetupNavigationResult(this MockNavigationService mockNavigationService, string destination, bool cancelled, Exception exception, INavigationParameters? navigationParameters = null)
+    {
+        SetupNavigationResultInternal(mockNavigationService, destination, false, navigationParameters, exception, cancelled);
+    }
+
+    private static void SetupNavigationResultInternal(MockNavigationService mockNavigationService, string destination, bool success, INavigationParameters? navigationParameters = null, Exception? navigationException = null, bool? cancelled = null)
     {
         var mockResult = new Mock<INavigationResult>();
 
         mockResult.SetupGet(m => m.Success)
             .Returns(success);
+
+        if (navigationException is not null)
+        {
+            mockResult.SetupGet(m => m.Exception)
+                .Returns(navigationException);
+        }
+
+        if (cancelled.HasValue)
+        {
+            mockResult.SetupGet(m => m.Cancelled)
+                .Returns(cancelled.Value);
+        }
 
         var destinations = DestinationHelper.GetValidDestinations(destination);
 
         foreach (var validDestination in destinations)
         {
-            mockNavigationService.Setup(
-                m => m.NavigateAsync(
-                    It.Is<Uri>(u => u.Equals(validDestination)),
-                    It.Is<INavigationParameters>(n => EquivalenceHelper.Equivalent(n, navigationParameters))))
-                .ReturnsAsync(mockResult.Object);
+            if (navigationParameters is null)
+            {
+                mockNavigationService.Setup(
+                    m => m.NavigateAsync(
+                        It.Is<Uri>(u => u.Equals(validDestination)),
+                        It.IsAny<INavigationParameters>())) // This will be null
+                    .ReturnsAsync(mockResult.Object);
+            }
+            else
+            {
+                mockNavigationService.Setup(
+                    m => m.NavigateAsync(
+                        It.Is<Uri>(u => u.Equals(validDestination)),
+                        It.Is<INavigationParameters>(n => EquivalenceHelper.Equivalent(n, navigationParameters))))
+                    .ReturnsAsync(mockResult.Object);
+            }
         }
     }
 }
--- tests/MockNavigationService.UnitTests/Tests/Extensions/SetupExtensionTests.cs
+++ tests/MockNavigationService.UnitTests/Tests/Extensions/SetupExtensionTests.cs
@@ -1,8 +1,178 @@
-﻿namespace MockNavigationService.UnitTests.Tests.Extensions;
+﻿using MockNavigationService.Extensions;
+
+namespace MockNavigationService.UnitTests.Tests.Extensions;
 
 public class SetupExtensionTests
 {
+    #region Setup
+
+    private MockNavigationService sut;
+
 	public SetupExtensionTests()
 	{
+		sut = new MockNavigationService();
 	}
+
+    #endregion Setup
+
+    #region Tests
+
+    [Theory]
+    [InlineData("SomePage", "AnotherPage")]
+    public async Task SetupNavigationResult_SetupDestination_ShouldNoRegisterAlternateDestination(string destination, string alternateDestination)
+    {
+        // Arrange
+        sut.SetupNavigationResult(destination, true);
+
+        // Act
+        var navigationResult = await sut.NavigateAsync(alternateDestination);
+
+        // Assert
+        Assert.False(navigationResult.Success);
+    }
+
+    [Theory]
+    [InlineData("SomePage", true)]
+    [InlineData("SomePageViewModel", true)]
+    [InlineData("SomeViewModel", true)]
+    [InlineData("SomePage", false)]
+    [InlineData("SomePageViewModel", false)]
+    [InlineData("SomeViewModel", false)]
+    public async Task SetupNavigationResult_SimpleOverload_ShouldRegisterExtras(string destination, bool expectedSuccess)
+	{
+        // Arrange
+
+        // Act
+        sut.SetupNavigationResult(destination, expectedSuccess);
+
+        var navigationSuccess = await sut.NavigateAsync(destination);
+
+        // Assert
+        Assert.Equal(expectedSuccess, navigationSuccess.Success);
+    }
+
+    [Theory]
+    [InlineData("SomePage", true)]
+    [InlineData("SomePageViewModel", true)]
+    [InlineData("SomeViewModel", true)]
+    [InlineData("SomePage", false)]
+    [InlineData("SomePageViewModel", false)]
+    [InlineData("SomeViewModel", false)]
+    public async Task SetupNavigationResult_SimpleOverloadWithParameters_ShouldRegisterExtras(string destination, bool expectedSuccess)
+    {
+        // Arrange
+
+        // Act
+        sut.SetupNavigationResult(destination, expectedSuccess);
+
+        var navigationSuccess = await sut.NavigateAsync(destination);
+
+        // Assert
+        Assert.Equal(expectedSuccess, navigationSuccess.Success);
+    }
+
+    [Fact]
+    public async Task SetupNavigationResult_WithException_ShouldReturnFailedNavigationResultWithDetail()
+    {
+        // Arrange
+        var destination = "SomePage";
+        var exception = new Exception("The maui navigation exploded");
+
+        // Act
+        sut.SetupNavigationResult(destination, exception);
+
+        var navigationResult = await sut.NavigateAsync(destination);
+
+        // Assert
+        Assert.False(navigationResult.Success);
+        Assert.Equal(exception, navigationResult.Exception);
+    }
+
+    [Fact]
+    public async Task SetupNavigationResult_WhenCancelledWithException_ShouldReturnFailedNavigationResultWithDetail()
+    {
+        // Arrange
+        var destination = "SomePage";
+        var exception = new Exception("The maui navigation exploded");
+
+        // Act
+        sut.SetupNavigationResult(destination, true ,exception);
+
+        var navigationResult = await sut.NavigateAsync(destination);
+
+        // Assert
+        Assert.False(navigationResult.Success);
+        Assert.Equal(exception, navigationResult.Exception);
+        Assert.True(navigationResult.Cancelled);
+    }
+
+    [Fact]
+    public async Task SetupNavigationResult_WithExceptionAndParameters_ShouldReturnFailedNavigationResultWithDetail()
+    {
+        // Arrange
+        var destination = "SomePage";
+        var exception = new Exception("The maui navigation exploded");
+        var navParams = new NavigationParameters()
+        {
+            { "Username", "Axemasta" },
+            { "Score", 123 },
+        };
+
+        // Act
+        sut.SetupNavigationResult(destination, exception, navParams);
+
+        var navigationResult = await sut.NavigateAsync(destination, navParams);
+
+        // Assert
+        Assert.False(navigationResult.Success);
+        Assert.Equal(exception, navigationResult.Exception);
+    }
+
+    [Fact]
+    public async Task SetupNavigationResult_WhenCancelledWithExceptionAndParameters_ShouldReturnFailedNavigationResultWithDetail()
+    {
+        // Arrange
+        var destination = "SomePage";
+        var exception = new Exception("The maui navigation exploded");
+        var navParams = new NavigationParameters()
+        {
+            { "Username", "Axemasta" },
+            { "Score", 123 },
+        };
+
+        // Act
+        sut.SetupNavigationResult(destination, true, exception, navParams);
+
+        var navigationResult = await sut.NavigateAsync(destination, navParams);
+
+        // Assert
+        Assert.False(navigationResult.Success);
+        Assert.Equal(exception, navigationResult.Exception);
+        Assert.True(navigationResult.Cancelled);
+    }
+
+    [Fact]
+    public async Task SetupNavigationResult_WhenSetupButCallingAlternativeNavigation_ShouldReturnGenericResult()
+    {
+        // Arrange
+        var destination = "SomePage";
+        var exception = new Exception("The maui navigation exploded");
+        var navParams = new NavigationParameters()
+        {
+            { "Username", "Axemasta" },
+            { "Score", 123 },
+        };
+
+        // Act
+        sut.SetupNavigationResult(destination, true, exception, navParams);
+
+        var navigationResult = await sut.NavigateAsync(destination);
+
+        // Assert
+        Assert.False(navigationResult.Success);
+        Assert.Null(navigationResult.Exception);
+        Assert.False(navigationResult.Cancelled);
+    }
+
+    #endregion Tests
 }
